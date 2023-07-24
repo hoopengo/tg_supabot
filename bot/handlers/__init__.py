a6a3adcbcaa1249c69144f31048f663b82476695
hoopengo <hoopengo@yandex.ru>
2023-07-24 19:22:41 +0300

    change /start command response, add tools handler

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,9 +1,11 @@
 """Import all routers and add them to routers_list."""
 from bot.handlers.start import start_router
+from bot.handlers.tools import tools_router
 
-routers_list = [
+routers_list = (
     start_router,
-]
+    tools_router,
+)
 
 __all__ = [
     "routers_list",

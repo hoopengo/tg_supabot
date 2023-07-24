bca3f8a27412fb041cc96a526f96942e71b1ae06
hoopengo <hoopengo@yandex.ru>
2023-07-24 22:05:04 +0300

    delete tools handler. add sticker handler

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,10 +1,10 @@
 """Import all routers and add them to routers_list."""
 from bot.handlers.start import start_router
-from bot.handlers.tools import tools_router
+from bot.handlers.sticker import sticker_router
 
 routers_list = (
     start_router,
-    tools_router,
+    sticker_router,
 )
 
 __all__ = [
--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -4,8 +4,13 @@ from aiogram.types import Message
 
 start_router = Router()
 
+start_sticker_id = "CAACAgIAAxkBAAMfZL6iDIJXXZfl7dd6b_5cDj13Fc8AAlY1AAIqiqFJG21rvpgD4GQvBA"
+
 
 @start_router.message(CommandStart(ignore_case=True, ignore_mention=True))
 async def _start(message: Message):
-    await message.answer_sticker("CAACAgIAAxkBAAMfZL6iDIJXXZfl7dd6b_5cDj13Fc8AAlY1AAIqiqFJG21rvpgD4GQvBA")
+    # send start sticker
+    await message.answer_sticker(start_sticker_id)
+
+    # send start message
     await message.answer("<b>Привет, акулёнок!</b> Снизу ты увидишь кнопочки, не хочешь их потыгыдыкать? 💕")
